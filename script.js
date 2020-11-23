7b59005aca4d04e1edc1e292f98281f7b0f9206b
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2020-11-23 06:08:00 +0600

    AOS animation on About Page

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('ready');
+AOS.init();
+//USING A NEW LIBRARY 
+
+
+
+const standoutText = document.querySelector('#standout');
+
+
+console.log(standoutText);
+
+
+document.addEventListener('aos:in', ({ detail }) => {
+    console.log('animated in', detail);
+  });
+  
+  document.addEventListener('aos:out', ({ detail }) => {
+    console.log('animated out', detail);
+  });
